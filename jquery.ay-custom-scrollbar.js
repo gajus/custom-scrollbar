b3e530eb4da1020344ffdd348481c7f97dbc7a30
Gajus Kuizinas <g.kuizinas@anuary.com>
2013-02-04 11:50:45 +0000

    Updated jQuery dependency. Added onScroll callback.

--- jquery.ay-custom-scrollbar.js
+++ jquery.ay-custom-scrollbar.js
@@ -1,5 +1,5 @@
 /**
- * jQuery custom-scrollbar v0.1.1
+ * jQuery custom-scrollbar v0.1.3
  * https://github.com/gajus/custom-scrollbar
  *
  * Licensed under the BSD.
@@ -14,25 +14,40 @@
 	
 	$.ay = $.ay || {};
 	
-	$.ay.customScrollbar	= function(options) {
+	$.ay.customScrollbar = function(options) {
 		if (!options.scrollbar) {
 			throw 'Missing scrollbar selector.';
 		} else if (!options.container) {
 			throw 'Missing container selector.';
 		}
 		
-		var sl = {
+		var handle_position = 0,
+			top,
+			client_y,
+			mousedown,
+			sl,
+			handle_height,
+			scrollbar_height,
+			content,
+			ratio,
+			disabled,
+			mousemoveEvent,
+			body = $('body');
+		
+		if (!options.onScroll) {
+			options.onScroll = function () {};
+		}
+		
+		sl = {
 			wrapper: options.container.find('.overflow-scroll'),
 			scrollbar: options.scrollbar,
 			handle: options.scrollbar.find('.handle'),
 		};
 		
-		var handle_height = sl.handle.height(),
-			scrollbar_height = sl.scrollbar.height(),
-			content = sl.wrapper.find('>'),
-			ratio,
-			disabled;
-		
+		handle_height = sl.handle.height();
+		scrollbar_height = sl.scrollbar.height();
+		content = sl.wrapper.find('>');
+				
 		options.container.on('ay-content-change', function () {		
 			var real_scrollbar_height = scrollbar_height-handle_height,
 				scrollable_height = content.height()-sl.wrapper.height();
@@ -48,26 +63,23 @@
 			}
 		}).trigger('ay-content-change');
 		
-		
-		var handle_position = 0,
-			top,
-			client_y,
-			mousedown;
-		
 		sl.wrapper.on('scroll', function(e){
+			var offset	= $(this).scrollTop();
+			
+			options.onScroll(offset);
+		
 			if(mousedown) {
 				return;
 			}
-		
-			var offset	= $(this).scrollTop();
 			
 			handle_position	= offset/ratio;
 			
 			sl.handle.css({top: handle_position});
 		});
 		
-		var mousemove_event		= function (e) {		
-			var move	= handle_position+e.clientY-client_y;
+		mousemoveEvent = function (e) {		
+			var move = handle_position+e.clientY-client_y,
+				scrollTop;
 				
 			if (move <= 0) {
 				top	= 0;
@@ -77,11 +89,13 @@
 				top	= move;
 			}
 			
-			sl.wrapper.scrollTop(top*ratio);
+			scrollTop = top*ratio;
+			
+			options.onScroll(scrollTop);
+			
+			sl.wrapper.scrollTop(scrollTop);
 			sl.handle.css({top: top});
 		};
-		
-		var body = $('body');
 		
 		sl.handle.on('mousedown', function (e) {
 			if (disabled) {
@@ -95,7 +109,7 @@
 			$(document).on('mouseup', function(e){
 				body.removeClass('ay-custom-scrollbar-in-action');
 			
-				$(document).off('mousemove', mousemove_event);
+				$(document).off('mousemove', mousemoveEvent);
 				
 				handle_position	= top;
 				mousedown		= false;
@@ -103,7 +117,7 @@
 			
 			client_y		= e.clientY;
 			
-			$(document).on('mousemove', mousemove_event);				
+			$(document).on('mousemove', mousemoveEvent);				
 		});
 	};
 })($)
